3e62259b3faa7cc10506fd8f863e57d284c03a4e
Hyadawild <kudosinichi9290@gmail.com>
2024-12-15 02:15:14 +0700

    add videoRT

---
+++ Release/video.bat
@@ -0,0 +1,7 @@
+@echo off
+/Users/KOMPUTER/Documents/ml/AyayaUpscale/Release/start.cmd --model upscayl-ultrasharp-v2 ^
+	--scale 4 ^
+    --format png ^
+    --video ^
+	--input ./GameTest.mp4 ^
+    --output my-video-upscaled.mp4
